0302f2315d901d1d347c8aa8abc8e1e7f9902721
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-13 14:04:50 +0530

    creating contact page and database

--- cite/shop/views.py
+++ cite/shop/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-from . models import Product
+from . models import Product , Contact
 from math import ceil
+import logging 
+
+# get an instance of a logger 
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
@@ -21,6 +25,14 @@ def about(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        print(request)
+        name = request.POST.get('name' , '')
+        email = request.POST.get('email' , '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name = name , email = email , phone = phone ,desc =  desc)
+        contact.save()
     return render(request , 'shop/contact.html')
 
 
